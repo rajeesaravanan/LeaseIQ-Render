434c0786c38ec6fa8609966cbb2f7bec52146deb
Rajee Saravanan <rajeesaravanan160801@gmail.com>
2025-12-30 20:57:48 +0530

    Add allowed origin for production environment

--- src/config/cors.js
+++ src/config/cors.js
@@ -4,7 +4,7 @@ const corsOptions = {
   origin: (origin, callback) => {
     if (!origin) return callback(null, true);
 
-    const allowedOrigins = ["http://localhost:3000", "http://localhost:5173"];
+    const allowedOrigins = ["http://localhost:3000", "http://localhost:5173","https://leaseiq-phase1.vercel.app"];
 
     if (allowedOrigins.includes(origin)) {
       callback(null, true);
